da7279d0ad0e3fc4591a538e2f6f8bdcef3dbee9
Syrus Akbary <me@syrusakbary.com>
2015-10-08 00:35:31 -0700

    Improved admin and DEBUG info in prod

--- starwars/admin.py
+++ starwars/admin.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.core.exceptions import ValidationError
 
 from .models import (
@@ -19,7 +19,8 @@ classes = [People, Planet, Film, Starship, Vehicle, Species]
 class ModelAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         if not request.user.is_superuser or True:
-            raise ValidationError("Only superusers can change models")
+            messages.error(request, "Only superusers can change models")
+            return False
         return super(ModelAdmin, self).save_model(request, obj, form, change)
 
 for c in classes:
